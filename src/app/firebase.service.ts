fcb538b84b5af6071dce8e29f154a65b5f284326
Satit Porntepanon <st_satitza@hotmail.com>
2020-08-12 03:31:03 +0700

    move logic code to service

--- src/app/firebase.service.ts
+++ src/app/firebase.service.ts
@@ -3,24 +3,38 @@ import {AngularFirestore} from '@angular/fire/firestore';
 // @ts-ignore
 import {User} from './shared/user';
 
-
 @Injectable({
   providedIn: 'root'
 })
 export class FirebaseService {
 
+  userData: User = {};
+
   constructor(private firebase: AngularFirestore) {
+  }
+
+  loadData(firstname: string) {
+    return this.firebase.collection('user-information').doc(firstname).snapshotChanges();
   }
 
   // firebase ไม่ save ทับถ้า doc ชื่อซ้ำกัน
   createData(data: User) {
     return new Promise<any>((resolve, reject) => {
+
+      // tslint:disable-next-line:max-line-length
+      if (data.firstname === undefined || data.lastname === undefined || data.age === undefined || data.email === undefined || data.telephone === undefined) {
+        data.status = 'doing';
+      } else {
+        data.status = 'complete';
+      }
+
+      data.createBy = 'Administrator';
+      data.createDate = new Date();
       this.firebase
         .collection('user-information')
         .doc(data.firstname)
         .set(data)
         .then(res => {
-          console.log(res);
         }, err => reject(err));
     });
   }
--- src/app/form/register/register.component.ts
+++ src/app/form/register/register.component.ts
@@ -10,27 +10,10 @@ import {User} from '../../shared/user';
 })
 export class RegisterComponent implements OnInit {
 
-  userData: User = {};
-
-  constructor(private firebaseService: FirebaseService) {
+  constructor(public firebaseService: FirebaseService) {
   }
 
   ngOnInit(): void {
 
-  }
-
-  saveData() {
-    // tslint:disable-next-line:max-line-length
-    if (this.userData.firstname === undefined || this.userData.lastname === undefined || this.userData.age === undefined || this.userData.email === undefined || this.userData.telephone === undefined) {
-      this.userData.status = 'doing';
-    } else {
-      this.userData.status = 'complete';
-    }
-
-    this.userData.createBy = 'Administrator';
-    this.userData.createDate = new Date();
-    this.firebaseService.createData(this.userData).then(() => {
-      console.log('create complete');
-    });
   }
 }
